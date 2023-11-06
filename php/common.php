eb565c5584b27536e74b1c2bb0ca4673f5de273e
jpedro85 <pedro.games30@gmail.com>
2023-11-06 02:09:00 +0000

    feat: Added server-side verifications feat: Aded functions in common.php that can be useb by others

--- php/common.php
+++ php/common.php
@@ -1,10 +1,17 @@
 <?php
 global $link;
-$link= createDatabaseConnection();
+$link = createDatabaseConnection();
 global $current_page;
-$current_page = get_site_url().'/'.basename(get_permalink());
+$current_page = get_site_url() . '/' . basename(get_permalink());
 
-function createDatabaseConnection() {
+/**
+ * The function creates a database connection using the provided host, user, password, and database
+ * name.
+ *
+ * @return mysqli database connection object.
+ */
+function createDatabaseConnection()
+{
     $dbConnection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     if ($dbConnection->connect_error) {
         die('Connection failed: ' . $dbConnection->connect_error);
@@ -13,23 +20,62 @@ function createDatabaseConnection() {
     return $dbConnection;
 }
 
-function get_enum_values($connection, $table, $column )
+/**
+ * The function retrieves the values of an ENUM column in a MySQL database table.
+ *
+ * @param mysqli $connection The connection object that is used to connect to the database
+ * @param string $table The name of the table in the database from which you want to retrieve the enum values.
+ * @param string $column The name of the column in the table for which you want to retrieve the enum values.
+ * @return array of values from the specified column in the specified table.
+ */
+function get_enum_values($connection, $table, $column)
 {
     $query = " SHOW COLUMNS FROM `$table` LIKE '$column' ";
-    $result = mysqli_query($connection, $query );
-    $row = mysqli_fetch_array($result , MYSQLI_NUM );
+    $result = mysqli_query($connection, $query);
+    $row = mysqli_fetch_array($result, MYSQLI_NUM);
     #extract the values
     #the values are enclosed in single quotes
     #and separated by commas
     $regex = "/'(.*?)'/";
-    preg_match_all( $regex , $row[1], $enum_array );
+    preg_match_all($regex, $row[1], $enum_array);
     $enum_fields = $enum_array[1];
-    return( $enum_fields );
+    return ($enum_fields);
 }
 
+/**
+ * Checks whether the user that's accessing the page has the needed permissions to see its contents
+ * @param string $permission name of the permission needed
+ * @return bool true if the user has the needed permissions, false if the user doesn't have the needed permissions
+ */
 function doesUserHavePermission($permission)
 {
     return is_user_logged_in() && current_user_can($permission);
+}
+
+/**
+ * Checks whether there exists a certain value in a given column in given table
+ * @param mysqli $connection The connection object that is used to connect to the database
+ * @param string $value The value you want to check inside the table and column
+ * @param string $table The name of the table in the database from which you want to check the field
+ * @param string $column The name of the column you want to check the value
+ * @return bool
+ */
+function checkFieldExistsOnDatabase($connection, $value, $table, $column)
+{
+    $query = "SELECT {$column} FROM {$table} WHERE {$column} = '{$value}'";
+    $result = mysqli_query($connection, $query);
+    $numRows = mysqli_num_rows($result);
+
+    return ($numRows > 0);
+}
+
+function voltar_atras(){
+
+    echo "<script type='text/javascript'>document.write(\"<a href='javascript:history.back()'> <button class='continueButton' >Voltar Atrás</button> </a>\");</script>
+    <noscript>
+    <a href='" . $_SERVER['HTTP_REFERER'] . "</a>
+    </noscript>";
+
 }
 
 ?>
--- php/gestao-de-itens.php
+++ php/gestao-de-itens.php
@@ -1,12 +1,102 @@
 <?php
 require_once("custom/php/common.php");
+// TODO Css for the popup saying the invalid form inputs
+// TODO CSS for the error in queries
 
 if (!doesUserHavePermission("manage_items")) {
     echo "Não tem autorização para aceder a esta página";
 } else {
-    // Intitial State if there is no POST["estado"]
-    echo "<table>";
-    echo "<thead>
+    if (array_key_exists("estado", $_REQUEST) && $_REQUEST["estado"] == "inserir") {
+        // Show tittle for the new page state
+        echo "<h3>Gestão de itens - inserção</h3>";
+
+        // Server-side verifications can be tested with postman
+        $validForm = true;
+        $invalidFields = "";
+
+        // Trim the received values, so it has no spaces in the edges
+        $itemName = (isset($_REQUEST["itemName"])) ? trim($_REQUEST["itemName"]) : "";
+        $typeName = (isset($_REQUEST["typeName"])) ? trim($_REQUEST["typeName"]) : "";
+
+        // Data from database to check item type
+        $itemStates = get_enum_values($link, "item", "state");
+
+        // Check itemName received is empty or just numbers
+        if (empty($itemName) || is_numeric($itemName)) {
+            $validForm = false;
+            $invalidFields .= "<p>Nome do item é invalido</p>";
+        }
+        // Checks whether the item type received is valid or not
+        if (empty($typeName) || is_numeric($typeName) || !checkFieldExistsOnDatabase($link, $typeName, "item_type", "name")) {
+            $validForm = false;
+            $invalidFields .= "<p>Tipo de item é invalido</p>";
+        }
+        // Checks whether the item state received was valid or not
+        if (empty($_REQUEST["state"]) || !in_array($_REQUEST["state"], $itemStates)) {
+            $validForm = false;
+            $invalidFields .= "<p>Estado do item é invalido</p>";
+        }
+        // Checks if there were any errors in the server side verification
+        if (!$validForm) {
+            echo $invalidFields;
+        } else {
+            // Fetch the item type id with same name as
+            $itemTypeIdQuery = "SELECT id FROM item_type WHERE name = '{$typeName}'";
+            $itemTypeIdResult = mysqli_query($link, $itemTypeIdQuery);
+
+            // Checks if the query was successful
+            if (!$itemTypeIdResult) {
+                echo "Ocorreu um erro na busca do item_typed id: " . mysqli_error($link);
+            } else {
+                $itemTypeId = mysqli_fetch_assoc($itemTypeIdResult)["id"];
+                // Start transaction to insert items
+                if (!$_SESSION["Item_added"] && mysqli_begin_transaction($link)) {
+
+                    // Insert new item in the correct table
+                    $insertNewItemQuery = "INSERT INTO item (id, name, item_type_id, state) VALUES (NULL, '" . $itemName . "', '" . $itemTypeId . "', '" . $_REQUEST["state"] . "')";
+                    $insertNewItemResult = mysqli_query($link, $insertNewItemQuery);
+
+                    // Checks if the query was successful
+                    if (!$insertNewItemResult) {
+                        mysqli_rollback($link);
+                        echo "Ocorreu um erro na Inserção de dados: " . mysqli_error($link);
+                        voltar_atras();
+                    } else {
+                        echo "<table>
+                                    <tr>
+                                        <th>id</th>
+                                        <th>Nome</th>
+                                        <th>Item Type id</th>
+                                        <th>Estado</th>
+                                    </tr> 
+                                    <tr>
+                                        <td>" . mysqli_insert_id($link) . "</td>
+                                        <td>" . $_REQUEST["itemName"] . "</td>
+                                        <td>" . $_REQUEST["typeName"] . "</td>
+                                        <td>" . $_REQUEST["state"] . "</td>
+                                    </tr> 
+                              </table>
+                              <a href='$curtent_page'><button href='$current_page' >Continuar</button></a>";
+                        // Commit the transaction
+                        mysqli_commit($link);
+                        $_SESSION["itemAdded"] = true;
+                    }
+                } else {
+                    // Checks if item was added already so to not cause duplication when refreshing the page
+                    if ($_SESSION["itemAdded"]) {
+                        echo "O valor ja foi inserido";
+                    } else {
+                        echo "Ocorreu um erro na Inserção de dados: " . mysqli_error($link);
+                    }
+                }
+            }
+        }
+
+
+    } else {
+        // Initial State if there is no REQUEST["estado"]
+        echo "<table>";
+        echo "<thead>
             <tr>
                 <th>tipo de item</th>
                 <th>id</th>
@@ -16,60 +106,85 @@ if (!doesUserHavePermission("manage_items")) {
             </tr>
           </thead>";
 
-    echo "<tbody>";
-    // Query the item_types of the databse
-    $itemTypeQuery = "SELECT type.id, type.name AS typeName FROM item_type AS type ORDER BY type.name";
-    $itemTypeData = mysqli_query($link, $itemTypeQuery);
-    while ($itemType = mysqli_fetch_assoc($itemTypeData)) {
-        // Query to fetch all the items that are linked with the current item_type
-        $itemQuery = "SELECT item.id, item.name as itemName, item.state FROM item WHERE item.item_type_id = {$itemType["id"]} ORDER BY item.id ";
-        $itemData = mysqli_query($link, $itemQuery);
+        echo "<tbody>";
+        // Query the item_types of the databse
+        $itemTypeQuery = "SELECT type.id, type.name AS typeName FROM item_type AS type ORDER BY type.name";
+        $itemTypeData = mysqli_query($link, $itemTypeQuery);
+        // Check if query was successful
+        if (!$itemTypeData) {
+            echo ":" . mysqli_error($link);
+        } else {
+            while ($itemType = mysqli_fetch_assoc($itemTypeData)) {
+                // Query to fetch all the items that are linked with the current item_type
+                $itemQuery = "SELECT item.id, item.name as itemName, item.state FROM item WHERE item.item_type_id = {$itemType["id"]} ORDER BY item.id ";
+                $itemData = mysqli_query($link, $itemQuery);
+                if (!$itemData) {
+                    echo "Ocorreu um erro na consulta:" . mysqli_error($link);
+                } else {
 
-        // Checks if the item_type actually has items linked to him
-        // if not all columns after are unified and says there is no items and jumps current cycle
-        $itemTypeItemsCount = mysqli_num_rows($itemData);
-        if ($itemTypeItemsCount == 0) {
-            echo "<td colspan='5'> Não há itens </td>";
-            continue;
-        }
+                    // Checks if the item_type actually has items linked to him
+                    // if not all columns after are unified and outputs that there is no items and jumps current cycle
+                    $itemTypeItemsCount = mysqli_num_rows($itemData);
+                    if ($itemTypeItemsCount == 0) {
+                        echo "<td colspan='5'> Não há itens </td>";
+                        continue;
+                    }
 
-        // Starting to format the item_type "row" by using the $itemTypeItemsCount value for the rowspan
-        $itemTypeRows = "<tr> <td rowspan='{$itemTypeItemsCount}'>{$itemType["typeName"]}";
-        while ($item = mysqli_fetch_assoc($itemData)) {
-            $itemTypeRows .= "<td>{$item["id"]}</td>";
-            $itemTypeRows .= "<td>{$item["itemName"]}</td>";
-            $itemTypeRows .= "<td>{$item["state"]}</td>";
-            $itemAction = "";
-            // Checking whether the current item state is active or inactive to have the correct action of changing state
-            $item["state"] == "active" ? $itemAction = "<a href=''>[desativar]</a>" : $itemAction = "<a href=''>[ativar]</a>";
-            // Formatting last column to have the all actions corresponding to the item data
-            $itemTypeRows .= "<td>
+                    // Starting to format the item_type "row" by using the $itemTypeItemsCount value for the rowspan
+                    $itemTypeRows = "<tr> <td rowspan='{$itemTypeItemsCount}'>{$itemType["typeName"]}";
+                    while ($item = mysqli_fetch_assoc($itemData)) {
+                        $itemTypeRows .= "<td>{$item["id"]}</td>";
+                        $itemTypeRows .= "<td>{$item["itemName"]}</td>";
+                        $itemTypeRows .= "<td>{$item["state"]}</td>";
+                        $itemAction = "";
+                        // Checking whether the current item state is active or inactive to have the correct action of changing state
+                        $item["state"] == "active" ? $itemAction = "<a href=''>[desativar]</a>" : $itemAction = "<a href=''>[ativar]</a>";
+                        // Formatting last column to have the all actions corresponding to the item data
+                        $itemTypeRows .= "<td>
                                     <a href=''>[editar]</a>
                                     {$itemAction}
                                     <a href=''>[editar]</a>
                                 </td></tr>";
 
+                    }
+                    // Finally sending the formatted "row" to the page
+                    echo $itemTypeRows;
+                }
+            }
         }
-        // Finally sending the formatted "row" to the page
-        echo $itemTypeRows;
+        // Close the table
+        echo "</table>";
+        echo "<hr><h3>Gestao de itens - introdução</h3>";
+
+        // Start the form to be able to create and add new items
+        echo "<form method='post' action='{$current_page}'>";
+
+        // The input for the Name of the item and its type
+        echo "<h5>Nome do Item</h5>
+        <input type='text' name='itemName' id='itemName' placeholder='Ex.: medidas, cabelo, autismo ...'>
+        <h5>Tipo de item</h5>";
+
+        // Reusing the Data fetched before to fill the radio buttons
+        $itemType = null;
+        foreach ($itemTypeData as $itemType) {
+            echo "<li><input type='radio' name='typeName' id='typeName' value='{$itemType["typeName"]}'>{$itemType["typeName"]}</li>";
+        }
+
+        // Input for the item State using the enums from the Database
+        echo "<h5>Estado do Item</h5>";
+        $itemStateValues = get_enum_values($link, "item", "state");
+        foreach ($itemStateValues as $itemState) {
+            echo "<li><input type='radio' name='state' id='state' value='{$itemState}'>{$itemState}</li>";
+        }
+
+        // Hidden input to specify which state of the page im in
+        echo "<input type='hidden' name='estado' value='inserir'>
+          <hr><button type='submit'>Submeter</button>";
+
+        // Initialize the session variable to be able to check if item was already added to DB or not
+        $_SESSION["itemAdded"] = false;
     }
-    // Close the table
-    echo "</table>";
-    echo "<h3>Gestao de itens - introdução</h3>";
-    // Start the form to be able to create and add new items
-    echo "<form method='post' action='{$current_page}'>";
-    // The input for the Name of the item
-    echo "<p>Nome do Item</p>
-          <input type='text' name='itemName' id='itemName' placeholder='Ex.: medidas, cabelo, autismo ...'>
-          <p>Tipo de item</p>";
-    $itemType = null;
-    foreach ($itemTypeData as $itemType){
-        echo "<li><input type='radio' name='{$itemType["typeName"]}' id='typeName'>{$itemType["typeName"]}</li>";
-    }
-    echo "<p>Estado do Item</p>";
-    $itemStateValues = get_enum_values($link,"item","state");
 
 }
-
 
 ?>
